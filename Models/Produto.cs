53f67b1624277fcc92c42650638d6cd2ca94cd41
Afonso -aptsharp <aff-on@hotmail.com>
2020-03-16 19:37:27 -0300

    Configurações Iniciais.

---
+++ Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC_DIO.Models
+{
+    public class Produto
+    {
+
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
